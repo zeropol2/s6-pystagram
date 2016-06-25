cebbf7332697ebd48c67b1a7136d889fc8b8446d
Kay Cha <kay@hannal.net>
2016-06-25 18:00:40 +0900

    photos 앱 추가

---
+++ photos/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.conf import settings
+
+
+class Photo(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    image = models.ImageField(upload_to='%Y/%m/%d/')
+    description = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
