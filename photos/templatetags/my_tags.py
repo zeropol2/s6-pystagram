cb9f9930583ae861a8644e8d0cca5e67b2cff346
zeropol2 <zeropol2@gmail.com>
2016-07-02 15:37:05 +0900

    [홍창우] custom tag 실습

--- photos/templatetags/my_tags.py
+++ photos/templatetags/my_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.template.base import VariableNode
+from django.contrib.auth import get_user_model
 
 
 register = template.Library()
@@ -21,3 +23,32 @@ def helloworld(*args, **kwargs):
     msg.append('</ul>')
 
     return ''.join(msg)
+
+
+@register.tag(name='addnim')
+def add_nim(parser, token):
+    nodelist = parser.parse(('endaddnim', 'end_add_nim', ))
+    parser.delete_first_token()
+    return NimNode(nodelist)
+
+
+class NimNode(template.Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+        self.user_class = get_user_model()
+
+    def render(self, context):
+        output = []
+
+        for node in self.nodelist:
+            if not isinstance(node, VariableNode):
+                output.append(node.render(context))
+                continue
+
+            obj = node.filter_expression.resolve(context)
+            if not isinstance(obj, self.user_class):
+                output.append(node.render(context))
+                continue
+
+            output.append('{}님'.format(node.render(context)))
+        return ''.join(output)
