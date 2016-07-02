421ba5dd1f3703222435bab24b788bcd1112a25c
zeropol2 <zeropol2@gmail.com>
2016-07-02 15:15:02 +0900

    [홍창우] simple tag 실습

--- photos/templatetags/my_tags.py
+++ photos/templatetags/my_tags.py
@@ -10,3 +10,14 @@ def did_like(photo, user):
     # return Like.objectes.filter(photo=photo, user=user, status=True).exists()
 
 
+@register.simple_tag
+def helloworld(*args, **kwargs):
+    msg = []
+    msg.append(', '.join([str(a) for a in args]))
+
+    msg.append('<ul>')
+    for k, v in kwargs.items():
+        msg.append('<li>{} : {} </li>'.format(k, v))
+    msg.append('</ul>')
+
+    return ''.join(msg)
