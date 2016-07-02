35789fc988672f8b25e35cd94fdb44fe9d89f61d
zeropol2 <zeropol2@gmail.com>
2016-07-02 17:36:36 +0900

    [홍창우] signal 실행

--- photos/models.py
+++ photos/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.core.urlresolvers import reverse_lazy
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 
 
 class Photo(models.Model):
@@ -11,6 +13,9 @@ class Photo(models.Model):
     description = models.TextField(max_length=500, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.pk)
 
     class Meta:
         ordering = ['-created_at', '-pk']  # 정렬조건
@@ -25,3 +30,9 @@ class Like(models.Model):
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+@receiver(post_delete, sender=Photo)
+def delete_attached_image(sender, **kwargs):
+    instance = kwargs.pop('instance')
+    instance.image.delete(save=False)
